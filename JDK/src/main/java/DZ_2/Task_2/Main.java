ea418e5335a1cfc4db6997e91966300362796880
Michael Goncharov <cadetvire@rambler.ru>
2026-02-25 18:57:43 +0300

    DZ_2 Task_2 Done

--- JDK/src/main/java/DZ_2/Task_2/Main.java
+++ JDK/src/main/java/DZ_2/Task_2/Main.java
@@ -5,21 +5,21 @@ public class Main {
         // 1. Работа с числами (Integer)
         Pair<Integer> intPair = new Pair<>(10, 20);
         System.out.println("Пара чисел: " + intPair);
-        System.out.println("Сумма: " + sum(intPair));
+        System.out.println("Сумма: " + GenericTask.sum(intPair));
 
         System.out.println("---");
 
         // 2. Работа с числами с плавающей точкой (Double)
         Pair<Double> doublePair = new Pair<>(5.5, 4.5);
         System.out.println("Пара double: " + doublePair);
-        System.out.println("Сумма: " + sum(doublePair));
+        System.out.println("Сумма: " + GenericTask.sum(doublePair));
 
         System.out.println("---");
 
         // 3. Работа со строками
         Pair<String> stringPair = new Pair<>("Hello", "Java!");
         System.out.println("Пара строк: " + stringPair);
-        System.out.println("Конкатенация: " + concatenate(stringPair));
+        System.out.println("Конкатенация: " + GenericTask.concatenate(stringPair));
 
         // Попытка вызвать sum для строк вызовет ошибку компиляции, что обеспечивает безопасность типов
         // sum(stringPair); // Ошибка: String не наследует Number
--- JDK/src/main/java/DZ_2/Task_2/Pair.java
+++ JDK/src/main/java/DZ_2/Task_2/Pair.java
@@ -1,25 +1,20 @@
 package DZ_2.Task_2;
 
+import lombok.Getter;
+
 /**
  * Обобщенный класс "Пара", хранящий два объекта одного типа.
  * @param <T> тип объектов в паре
  */
 
-public class Pair {
+@Getter
+public class Pair <T> {
     private final T first;
     private final T second;
 
     public Pair(T first, T second) {
         this.first = first;
         this.second = second;
-    }
-
-    public T getFirst() {
-        return first;
-    }
-
-    public T getSecond() {
-        return second;
     }
 
     @Override
